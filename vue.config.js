dcc8f189d162600a40a619516e4af694a1bfa0ac
paratrooper-94 <ermilov1994@yandex.ru>
2022-03-15 15:05:19 +0300

    new

--- vue.config.js
+++ vue.config.js
@@ -3,8 +3,14 @@ module.exports = defineConfig({
   transpileDependencies: true
 })
 
+// module.exports = {
+//   publicPath: process.env.NODE_ENV === 'production'
+//     ? '/GB__Vue.js_from-24.02.22/gb__vue-1.1-calculator/'
+//     : '/'
+// }
+
 module.exports = {
   publicPath: process.env.NODE_ENV === 'production'
-    ? '/GB__Vue.js_from-24.02.22/gb__vue-1.1-calculator/'
-    : '/'
+  ? '/calculator__Vue/'
+  : '/'
 }
